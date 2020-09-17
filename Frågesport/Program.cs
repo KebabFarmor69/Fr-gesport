250b6ec5f41fcd7353a72bf108b1dee2e1c714ae
Emma Gummeson <emma.gummeson@learnet.se>
2020-09-17 09:48:46 +0200

    Frågesport 1

---
+++ Frågesport/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the questions of the universe!\n\nToday, you're going to answer a few questions and if you´re right... " +
+            "\n\nCONGRATS TO YOU!"
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
